d9489159efe9c11cd3db0b9eba8027e0469edf16
Satoshi Fukutomi <info@fuktommy.com>
2008-03-21 23:45:16 +0000

    はてブで「ブックマークを確認」にチェックを入れるuser.js
    git-svn-id: http://repos.fuktommy.com/svn/main/fuktommy.com/trunk@11 5ff462cd-d70c-4577-abd0-42c2245143fb

---
+++ js/hatebu_check_gobm.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Hanena Bookmark Check Go Bookmark
+// @namespace   http://fuktommy.com/js/
+// @description Check go bookmark checkbox in hatena bookmark add entry page.
+// @include     http://b.hatena.ne.jp/add?mode=confirm&*
+// ==/UserScript==
+
+// Copyright (c) 2008 Satoshi Fukutomi <info@fuktommy.com>.
+// http://fuktommy.com/js/hatebu_check_gobm.user.js
+// Distributed under the new BSD license
+// http://fuktommy.com/bsdl
+// $Id$
+
+(function() {
+    function checkGoBookmark() {
+        document.getElementById('go_bm').checked = true;
+    }
+
+    window.addEventListener('load', checkGoBookmark, false);
+})();

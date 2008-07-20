314ef02fa274268a9fef281923bab5dad023bac5
Satoshi Fukutomi <info@fuktommy.com>
2008-07-20 15:32:19 +0000

    2chの検索リンク修正を追加。
    git-svn-id: http://repos.fuktommy.com/svn/main/fuktommy.com/trunk@77 5ff462cd-d70c-4577-abd0-42c2245143fb

---
+++ js/2ch_searchlink.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        2ch Search Link Modifier
+// @namespace   http://fuktommy.com/js/
+// @description Accent recent userd tag at add entry page.
+// @include     http://find.2ch.net/*
+// ==/UserScript==
+
+// Copyright (c) 2008 Satoshi Fukutomi <info@fuktommy.com>.
+// http://fuktommy.com/js/
+// Distributed under new BSD license
+// http://fuktommy.com/bsdl
+// $Id: $
+
+(function() {
+    function modifyLinks() {
+        var links = document.getElementsByTagName('a');
+        for(var i=0; i<links.length; i++) {
+            links[i].href = links[i].href.replace(/[0-9]*-[0-9]*$/,'l50');
+        }
+    }
+
+    window.addEventListener('load', modifyLinks, false);
+})();

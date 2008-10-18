e39f8c4de338a471842df60e9080fa424871898d
Satoshi Fukutomi <info@fuktommy.com>
2008-10-18 08:52:39 +0000

    Amazonのロゴを黒にして目立たせる。
    git-svn-id: http://repos.fuktommy.com/svn/main/fuktommy.com/trunk@120 5ff462cd-d70c-4577-abd0-42c2245143fb

--- template/amazonwidget.h
+++ template/amazonwidget.h
@@ -3,9 +3,7 @@ amazon_ad_tag="fuktommy-22";
 amazon_ad_width="180"; 
 amazon_ad_height="150"; 
 amazon_color_background="EFEFEF"; 
-amazon_color_border="808080"; 
+amazon_color_border="000000"; 
 amazon_color_logo="FFFFFF"; 
-amazon_color_link="0000FF"; 
-amazon_ad_logo="hide"; 
-amazon_ad_title="最近読んだ本"; //--></script>
+amazon_color_link="0000FF"; //--></script>
 <script type="text/javascript" src="http://www.assoc-amazon.jp/s/asw.js"></script>

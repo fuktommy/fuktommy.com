2ecfe5e3517d844942b95876caac7a0585506f71
Satoshi Fukutomi <info@fuktommy.com>
2008-12-13 06:56:43 +0000

    FAQ追加。
    git-svn-id: http://repos.fuktommy.com/svn/main/hateber.fuktommy.com/trunk@148 5ff462cd-d70c-4577-abd0-42c2245143fb

--- setup.sh
+++ setup.sh
@@ -3,4 +3,7 @@
 cd /srv/stage/hateber.fuktommy.com
 
 rsync -Cacv --delete \
+    --exclude="- /*.ht" \
+    --exclude="- /setup.sh" \
+    --exclude="- /template" \
     ./ /srv/www/hateber.fuktommy.com/
---
+++ template/head.h
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <meta http-equiv="content-script-type" content="text/javascript" />
+>>>ifdef TOPPAGE
+  <title><$TITLE></title>
+>>>else
+  <title><$TITLE> - はてバーぶろぐ</title>
+>>>endif
+  <meta name="description" content="<$DESCRIPTION>" />
+  <link rev="made" href="mailto:webmaster@fuktommy.com" />
+  <link rel="contents" href="/" title="トップ" />
+  <link rel="stylesheet" type="text/css" href="/common.css" />
+  <link rel="stylesheet" type="text/css" href="/pc.css" media="screen" />
+  <link rel="meta" type="application/rdf+xml" title="license" href="/license" />
+</head>
+<body>
+>>>ifdef TOPPAGE
+  <h1><$TITLE></h1>
+>>>else
+  <h1><$TITLE> - <a href="/">はてバーぶろぐ</a></h1>
+>>>endif

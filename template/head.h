2ed641417295795e733aa6f5173db39418f42254
Satoshi Fukutomi <info@fuktommy.com>
2011-11-02 16:43:52 +0000

    HTML5対応
    git-svn-id: http://repos.fuktommy.com/svn/main/fuktommy.com/trunk@433 5ff462cd-d70c-4577-abd0-42c2245143fb

--- template/head.h
+++ template/head.h
@@ -1,20 +1,17 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
 <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-  <meta http-equiv="content-script-type" content="text/javascript" />
   <title><$TITLE></title>
   <meta name="description" content="<$DESCRIPTION>" />
-  <link rev="made" href="mailto:webmaster@fuktommy.com" />
+  <link rel="author" href="mailto:webmaster@fuktommy.com" />
   <link rel="contents" href="/" title="トップ" />
 >>>ifdef SECTION
   <link rel="contents" href="./" title="<$SECTION>" />
 >>>endif
   <link rel="alternate" type="application/rss+xml" title="RSS" href="/rss" />
   <link rel="alternate" type="application/atom+xml" title="Blog Atom Feed" href="http://feeds.feedburner.com/fuktommy" />
-  <link rel="meta" type="application/rdf+xml" title="license" href="/license" />
+  <link rel="license" type="application/rdf+xml" href="/license" />
 >>>ifdef TOPPAGE
   <link rel="stylesheet" type="text/css" href="/topcommon.css" />
   <link rel="stylesheet" type="text/css" href="/toppc.css" media="screen" />

25ac876c3d3db6de77b153d5aef57fa4ac049bee
Satoshi Fukutomi <info@fuktommy.com>
2020-12-29 23:37:34 +0900

    広告変更

--- fuktommy.com/template/adsfoot.h
+++ fuktommy.com/template/adsfoot.h
@@ -1,10 +1,10 @@
 <script type='text/javascript'>
 var amzn_wdgt={widget:'MyFavorites'};
 amzn_wdgt.tag='fuktommy-22';
-amzn_wdgt.columns='1';
+amzn_wdgt.columns='4';
 amzn_wdgt.rows='1';
 amzn_wdgt.title='広告';
-amzn_wdgt.width='150';
+amzn_wdgt.width='450';
 amzn_wdgt.ASIN='B08BGBZT16,B07QMYTRR9,B06W5528D5,B074CGZ1XN,B01HAWP672,4040685520,B01N3XV7YK,B07P83Y4K2,B07YVGD8WG,B08KPXXTH6,B084KKPJ2P,B084QJGSVH';
 amzn_wdgt.showImage='True';
 amzn_wdgt.showPrice='True';
